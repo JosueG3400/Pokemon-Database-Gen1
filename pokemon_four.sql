0e6a7d003070b263ed324ccbd90a54fde28f423a
Josue Guerrero <46791444+JosueG3400@users.noreply.github.com>
2021-08-19 20:20:15 -0700

    Update pokemon_four.sql

--- pokemon_four.sql
+++ pokemon_four.sql
@@ -1,29 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Aug 03, 2021 at 01:16 AM
--- Server version: 5.7.24
--- PHP Version: 7.4.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `pokemon_four`
---
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `evolutions`
 --
@@ -847,7 +821,3 @@ ALTER TABLE `pokemon_types`
   ADD CONSTRAINT `fk_types1_id` FOREIGN KEY (`types1_id`) REFERENCES `types` (`type_id`),
   ADD CONSTRAINT `fk_types2_id` FOREIGN KEY (`types2_id`) REFERENCES `types` (`type_id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
